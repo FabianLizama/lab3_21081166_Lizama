f815678bdfe7bdb9996bfb408b61818a7f58e7f7
Fabián Lizama <fabian.lizama@usach.cl>
2022-12-03 23:42:39 -0300

    Se termina la documentación del código

--- PixHex_21081166_LizamaFabian.java
+++ PixHex_21081166_LizamaFabian.java
@@ -1,20 +1,38 @@
 public class PixHex_21081166_LizamaFabian extends Pixel_21081166_LizamaFabian{
+    /**
+     * hexcode representa el color del pixel en formato hexadecimal, por ejemplo "a9ff00"
+     */
     public String hexcode;
-
+    /**
+     * Método constructor de Pixbit
+     * @param x coordenada x del pixel
+     * @param y coordenada y del pixel
+     * @param depth profundidad del pixel en el espacio
+     * @param hexcode color del pixel en formato hexadecimal
+     */
     public PixHex_21081166_LizamaFabian(int x, int y, int depth, String hexcode) {
         this.hexcode = hexcode;
         this.x = x;
         this.y = y;
         this.depth = depth;
     }
-
+    /**
+     * Selector del código hexadecimal del pixel
+     * @return codigo hexadecimal
+     */
     public String getHexcode() {
         return hexcode;
     }
-
+    /**
+     * Modificador del código hexadecimal del pixel
+     * @param hexcode  codigo hexadecimal a cambiar
+     */
     public void setHexcode(String hexcode) {
         this.hexcode = hexcode;
     }
+    /**
+     * Método que invierte el color rgb de un pixel
+     */
     public void pixHexInvertColorRGB() {
         int r = Integer.parseInt(this.getHexcode().substring(0,2),16);
         int g = Integer.parseInt(this.getHexcode().substring(2,4),16);
--- PixRGB_21081166_LizamaFabian.java
+++ PixRGB_21081166_LizamaFabian.java
@@ -1,8 +1,19 @@
 import java.util.ArrayList;
 
 public class PixRGB_21081166_LizamaFabian extends Pixel_21081166_LizamaFabian{
+    /**
+     * rgb es una lista que contiene la cantidad de cada canal r, g o b respectivamente
+     */
     public ArrayList<Integer> rgb = new ArrayList<>();
-
+    /**
+     * Método constructor de Pixbit
+     * @param x coordenada x del pixel
+     * @param y coordenada y del pixel
+     * @param depth profundidad del pixel en el espacio
+     * @param r cantidad de rojo que tiene el pixel
+     * @param g cantidad de verde que tiene el pixel
+     * @param b cantidad de azul que tiene el pixel
+     */
     public PixRGB_21081166_LizamaFabian(int x, int y, int depth, int r, int g, int b) {
         this.rgb.add(r);
         this.rgb.add(g);
@@ -11,13 +22,23 @@ public class PixRGB_21081166_LizamaFabian extends Pixel_21081166_LizamaFabian{
         this.y = y;
         this.depth = depth;
     }
+    /**
+     * Selector del color del pixel
+     * @return lista rgb con la información del color
+     */
     public ArrayList<Integer> getRgb() {
         return rgb;
     }
-
+    /**
+     * Modificador del color del pixel
+     * @param rgb lista rgb con la información del color
+     */
     public void setRgb(ArrayList<Integer> rgb) {
         this.rgb = rgb;
     }
+    /**
+     * Método que invierte el color rgb de un pixel
+     */
     public void pixRGBInvertColorRGB(){
         int r = this.getRgb().get(0);
         int g = this.getRgb().get(1);
